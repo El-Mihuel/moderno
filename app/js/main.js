9eac3999228e7db443fd5afd497a6ff9812bb047
El-Mihuel <khomamisha09@gmail.com>
2021-10-11 16:17:43 +0300

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -3,5 +3,11 @@ $(function(){
 
     var mixer = mixitup('.products__inner-box');
 
+    $('.product-slider__inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4
+    });
 
 });
