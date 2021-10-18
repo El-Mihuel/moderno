d64a450f66d038025f60a2b030c12dd92a9957d7
El-Mihuel <khomamisha09@gmail.com>
2021-10-18 16:32:53 +0300

    added product-page

--- app/js/main.js
+++ app/js/main.js
@@ -3,11 +3,22 @@ $(function(){
 
     var mixer = mixitup('.products__inner-box');
 
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        prefix: "$"
+    });
+
     $('.product-slider__inner').slick({
         dots: true,
         arrows: false,
         slidesToShow: 4,
         slidesToScroll: 4
     });
+
+    
 
 });
